31d2ea33c8da455a577fa077dd2a0475a5bd19fc
Belen Rosas <belenrosas97@gmail.com>
2023-11-09 00:44:25 -0300

    Comienzo de la traduccion en el main

---
+++ js/main.js
@@ -0,0 +1,157 @@
+import { urlBackoffice } from "../js/configuracion.js";
+import { ruta } from "./variables.js";
+
+let aplicacionActual = "Almacen";
+
+const flechaIzquierda = document.getElementById("izquierda");
+const flechaDerecha = document.getElementById("derecha");
+
+if (aplicacionActual === "Almacen"){
+  revelarAplicacionAlmacen();
+}
+
+function revelarAplicacionAlmacen(){
+  $("#nombreAplicacion").html("APLICACIÓN DE GESTIÓN <br> DE ALMACENES");
+  $("#informacionApp").html("Gestione los artículos de su almacén, así como los paquetes y lotes con los que trabajar.");
+  $("#accesoAplicacion").attr("href", "/html/aplicaciones/appAlmacenes.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGAlmacen.png)");
+
+  ocultarFlechaIzquierda();
+  mostrarFlechaDerecha();
+  accesoAplicacionCamiones(flechaDerecha);
+}
+
+function revelarAplicacionCamiones(){
+  $("#nombreAplicacion").html("APLICACIÓN DE GESTIÓN <br> DE CAMIONES");
+  $("#informacionApp").html("Observe el contenido de su vehículo, así como sus entregas pendientes y destinos a cargar.");
+  $("#accesoAplicacion").attr("href", "/html/aplicaciones/appCamiones.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGCamiones.png)");
+
+  mostrarFlechaIzquierda();
+  mostrarFlechaDerecha();
+  accesoAplicacionAlmacen(flechaIzquierda);
+  accesoAplicacionSeguimiento(flechaDerecha);
+}
+
+function revelarAplicacionSeguimiento(){
+  $("#nombreAplicacion").html("APLICACIÓN DE <br> SEGUIMIENTO");
+  $("#informacionApp").html("Observe la lista de paquetes con destinos establecidos, con su respectiva información asignada.");
+  $("#accesoAplicacion").attr("href", "/html/aplicaciones/appSeguimiento.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGSeguimiento.png)");
+
+  mostrarFlechaIzquierda();
+  mostrarFlechaDerecha();
+  accesoAplicacionCamiones(flechaIzquierda);
+  accesoBackoffice(flechaDerecha);
+}
+
+function revelarBackoffice(){
+  $("#nombreAplicacion").html("BACKOFFICE DE <br> ADMINISTRACIÓN");
+  $("#informacionApp").html("Gestione los Usuarios del Sistema, así como todos los Almacenes y Vehículos en disposición.");
+  $("#accesoAplicacion").attr("href", "/html/aplicaciones/appBackOffice.html");
+  $(".contenedorInformacion").css("background-image", "url(/img/BGBackoffice.png)");
+
+  mostrarFlechaIzquierda();
+  ocultarFlechaDerecha();
+  accesoAplicacionSeguimiento(flechaIzquierda);
+}
+
+function ocultarFlechaIzquierda(){
+  flechaIzquierda.style.display = "none";
+}
+
+function ocultarFlechaDerecha(){
+  flechaDerecha.style.display = "none";
+}
+
+function mostrarFlechaIzquierda(){
+  flechaIzquierda.style.display = "block";
+}
+
+function mostrarFlechaDerecha(){
+  flechaDerecha.style.display = "block";
+}
+
+function accesoAplicacionAlmacen(boton){
+  boton.addEventListener('click', function(){
+    revelarAplicacionAlmacen();
+  })
+}
+
+function accesoAplicacionSeguimiento(boton){
+  boton.addEventListener('click', function(){
+    revelarAplicacionSeguimiento();
+  })
+}
+
+function accesoAplicacionCamiones(boton){
+  boton.addEventListener('click', function(){
+    revelarAplicacionCamiones();
+  })
+}
+
+function accesoBackoffice(boton){
+  boton.addEventListener("click", function() {
+    revelarBackoffice();
+  });
+}
+
+$("#appAlmacen").click(function(){
+  revelarAplicacionAlmacen();
+});
+
+$("#appChoferes").click(function(){
+  revelarAplicacionCamiones();
+});
+
+$("#appSeguimiento").click(function(){
+  revelarAplicacionSeguimiento();
+});
+
+$("#appBackoffice").click(function(){
+  revelarBackoffice();
+});
+
+function aplicarIngles() {
+  document.cookie = "lang=en;path=/"
+  location.reload()
+}
+
+function aplicarEspanol(){
+  document.cookie = "lang=es;path=/"
+  location.reload()
+}
+
+$('#idiomaDelSistema').click(function(){
+  if(document.cookie.indexOf("lang=en") !== -1){
+      aplicarEspanol()
+  } else {
+      aplicarIngles()
+  }
+});
+
+/*$(document).ready(function () {
+  if(document.cookie.indexOf("lang=en") !== -1){
+      $('#idiomaDelSistema').css('background-image', 'url(/img/banderaUK.png)')
+  } else {
+      $('#idiomaDelSistema').css('background-image', 'url(/img/banderaUruguay.png)')
+  }
+  Promise.all([fetch('/' + ruta), fetch('/json/elementos.json')])
+  .then((responses) => Promise.all(responses.map((response) => response.json())))
+  .then((data) => {
+      const idioma = data[0];
+      const arrayDeIdioma = idioma[5]
+      const arrayDeTextos = data[1];
+      const arrayDeTextos2 = arrayDeTextos[5]
+
+      for (let posicion = 0; posicion < Object.keys(arrayDeTextos2).length; posicion++){
+          let texto = document.getElementById(arrayDeTextos2[posicion])
+          if (texto.nodeName == "INPUT"){
+              texto.placeholder = arrayDeIdioma[posicion]
+          } else {
+              texto.textContent = arrayDeIdioma[posicion]
+          }
+      }
+  })
+
+});*/
---
+++ php/correo.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+error_log("Inicio del script", 0);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $telefono = $_POST["telefono"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $destinatario = "belenrosas97@gmail.com";
+    $asunto = "Nuevo mensaje de formulario de contacto";
+
+    $contenido = "Nombre: $nombre\n"
+    .  "Teléfono: $telefono\n"
+    . "Correo electrónico: $email\n"
+    . "Mensaje:\n$mensaje\n";
+
+    if (mail($destinatario, $asunto, $contenido)) {
+        $response = array("success" => true);
+    } else {
+        $response = array("success" => false);
+    }
+
+    header("Content-Type: application/json");
+    echo json_encode($response);
+}
+
+error_log("Fin del script", 0);
+?>
+
